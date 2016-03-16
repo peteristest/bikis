8ca2b53df5eb9c8f51d4f6ebd4418cd0cc60b393
Peteris Bikis <peteris@asketicsf.com>
2016-03-16 15:14:17 +0000

    Added basscss

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,7 +12,7 @@ import { connect } from 'react-redux'
 class App extends Component {
   render () {
     return (
-      <div className='wrapper'>
+      <div className='wrapper px3 py2'>
         {this.props.children}
       </div>
     )
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -9,8 +9,8 @@ import { connect } from 'react-redux'
 class HomePage extends Component {
   render () {
     return (
-      <div>
-        <h1>peter.is</h1>
+      <div className='max-width-2'>
+        <h1 className='h3'>Peteris Bikis</h1>
         <p>
           Creative Technologist, Designer and Engineer. <a href='//asketicsf.com' target='_blank'>Asketic</a> Co-founder. I'm a venn diagram of design, technology, the Internet, cycling, travel, and photography. Currently obsessed with React and functional programming. Interested in AI, neural networks and a weirder future &#128126;
         </p>
