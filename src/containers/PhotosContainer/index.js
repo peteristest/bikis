7577b8ac5d33ed936786f739476f27b727ec41c9
Peteris Bikis <peteris@asketicsf.com>
2016-03-18 19:59:35 +0000

    Added feature photo, broke out Photo into a separate component

--- src/containers/PhotosContainer/index.js
+++ src/containers/PhotosContainer/index.js
@@ -6,11 +6,29 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { asyncFetchInstagramPhotos } from '../../actions'
 
-const NUM_PHOTOS = 9
+const NUM_PHOTOS = 5
 const DEGREES = 2
 const SIZE = 50
-const WIDTH = 55
-const HEIGHT = 50
+const WIDTH = 50
+const HEIGHT = 10
+const TRANSFORM_SMALL = 'TRANSFORM_SMALL'
+const TRANSFORM_LARGE = 'TRANSFORM_LARGE'
+
+const random = (int) => Math.floor(Math.random() * int)
+const shuffle = () => 0.5 - Math.random()
+
+const Photo = ({delay, transform, url}) => (
+  <span
+    className='image-container inline-block ml2 mt2 mr2 right-0 bottom-0 absolute'
+    style={{transitionDelay: `${delay}ms`}}>
+    <span className='image relative inline-block' style={{ transform }}>
+      <img
+        src={url}
+        className='fit relative block' />
+      <span className='bg absolute top-0 left-0 right-0 bottom-0' />
+    </span>
+  </span>
+)
 
 class PhotosContainer extends Component {
 
@@ -37,52 +55,52 @@ class PhotosContainer extends Component {
     })
   }
 
+  getTransform (size) {
+    switch (size) {
+      case TRANSFORM_SMALL:
+        return {
+          rotation: random(DEGREES) - DEGREES * 0.5,
+          scale: 1 + (random(SIZE) - SIZE * 0.5) * 0.01,
+          x: random(WIDTH),
+          y: random(HEIGHT)
+        }
+      case TRANSFORM_LARGE:
+        return {
+          rotation: 0,
+          scale: 3.4,
+          x: 2,
+          y: 0
+        }
+    }
+  }
+
   prepareImages (images) {
     return images
-      .sort(() => 0.5 - Math.random())
+      .sort(shuffle)
       .slice(0, NUM_PHOTOS)
-      .map((image, i) => {
-        const rotation = Math.floor(Math.random() * DEGREES) - DEGREES * 0.5
-        const scale = 1 + (Math.floor(Math.random() * SIZE) - SIZE * 0.5) * 0.01
-        const order = Math.floor(Math.random() * images.length)
-        const x = Math.floor(Math.random() * WIDTH)
-        const y = Math.floor(Math.random() * HEIGHT)
+      .map((url, i) => {
+        const isSmall = Boolean(i)
+        const size = isSmall ? TRANSFORM_SMALL : TRANSFORM_LARGE
+        const { rotation, scale, x, y } = this.getTransform(size)
+        const delay = isSmall ? random(images.length) : 0
 
         return {
-          url: image,
+          url,
           rotation,
           scale,
-          x,
-          y,
-          order
+          x, y,
+          delay
         }
       })
   }
 
-  renderImages (images, withoutDelay = false) {
+  renderPhotos (images, withoutDelay = false) {
     return images.map((image, i) => {
-      const { url, rotation, scale, x, y, order } = image
-      const delay = withoutDelay ? 0 : order * 15
-      const style = {
-        height: 'auto',
-        transform: `rotate(${rotation}deg) scale(${scale}) translateX(-${x}vw) translateY(-${y}vh)`
-      }
-      return (
-        <span
-          className='image-container inline-block ml2 mt2 mr2 right-0 bottom-0 absolute'
-          style={{
-            transitionDelay: `${delay}ms`,
-            width: '25%'
-          }}>
-          <span className='image relative inline-block' style={style}>
-            <img
-              key={i}
-              src={url}
-              className='fit relative block' />
-            <span className='bg absolute top-0 left-0 right-0 bottom-0' />
-          </span>
-        </span>
-      )
+      const { url, rotation, scale, x, y, delay } = image
+      const d = withoutDelay ? 0 : delay * 15
+      const transform = `rotate(${rotation}deg) scale(${scale}) translateX(-${x}vw) translateY(-${y}vh)`
+
+      return <Photo key={i} delay={d} transform={transform} url={url} />
     })
   }
 
@@ -91,7 +109,7 @@ class PhotosContainer extends Component {
 
     return (
       <div {...this.props}>
-        {this.renderImages(images, withoutDelay)}
+        {this.renderPhotos(images, withoutDelay)}
       </div>
     )
   }
