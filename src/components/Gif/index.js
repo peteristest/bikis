5004944741d55228227e66e8b9223d085c59af1d
Peteris Bikis <peteris@asketicsf.com>
2016-06-15 16:51:30 +0100

    Localised GIF assets

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './styles.css'
 
+import cloud from './img/cloud.gif'
+import technology from './img/technology.gif'
+import ai from './img/ai.gif'
+import future from './img/future.gif'
+
 const Gif = ({src, name}) => (
   <span className={`component gif ${name} fixed abs-center`}>
     <img className='gif-image' src={src} />
@@ -9,19 +14,19 @@ const Gif = ({src, name}) => (
 )
 
 export const TechnologyGif = () => (
-  <Gif name='technology' src='https://media.giphy.com/media/jy7Ipmx7Zeb0k/giphy.gif' />
+  <Gif name='technology' src={technology} />
 )
 
 export const WeirdGif = () => (
-  <Gif name='weird' src='https://media.giphy.com/media/QTKpNChuRixKo/giphy.gif' />
+  <Gif name='weird' src={future} />
 )
 
 export const AiGif = () => (
-  <Gif name='ai' src='https://media.giphy.com/media/IWoZqzqk7LZn2/giphy.gif' />
+  <Gif name='ai' src={ai} />
 )
 
 export const CloudGif = () => (
-  <Gif name='cloud' src='https://media.giphy.com/media/OT2lwSsUgpsT6/giphy.gif' />
+  <Gif name='cloud' src={cloud} />
 )
 
 export default Gif
